8b2431be8159c46306a776be675a3ba0aab7d287
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1661.local>
2021-11-24 11:33:38 +0000

    import and exporting

---
+++ app.js
@@ -0,0 +1,4 @@
+const { add } = require("./bensFunctions");
+// import { add, divide } from "./bensFunctions.js";
+
+console.log(add(5, 5));
---
+++ bensFunctions.js
@@ -0,0 +1,14 @@
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
+
+module.exports = {
+  add: add,
+  divide: divide,
+  name: "bensFunctions.js",
+  description: "These are some really cool functions",
+};
