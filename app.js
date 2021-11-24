fe36e8741a8c378bf54006dbabdd65fdb9839672
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1661.local>
2021-11-24 14:12:38 +0000

    inquierer demo

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
-const os = require("os");
-const fs = require("fs");
-const _ = require("lodash");
-const { add } = require("./bensFunctions");
-const math = require("./math");
-// import { add, divide } from "./bensFunctions.js";
+const inquirer = require("inquirer")
 
-console.log(math);
+const init = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "question1",
+        message: "Hello, what can I get for you today?",
+        choices: ["Latte", "Cappacino", "Mocha", "Flat White", "Water"],
+      },
+      {
+        type: "input",
+        name: "question2",
+        message: "May I take you name please?",
+      },
+    ])
+    .then((answers) => {
+      console.table(answers)
+    })
+    .then(() => gameLoop())
+}
+
+const gameLoop = () => {
+  // This is where you can run some recursion to make a loop
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "question1",
+        message: "This is a question that you will see again soon...",
+        choices: ["Woo", "yay", "see ya"],
+      },
+    ])
+    .then((answers) => {
+      console.table(answers)
+    })
+    // this line will trigger the function to run again
+    .then(() => gameLoop())
+}
+
+init()
